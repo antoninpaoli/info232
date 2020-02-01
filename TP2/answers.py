52ea45e631cee6a4bef3d94fda9afb3d6ed778c7
antoninpaoli <53855385+antoninpaoli@users.noreply.github.com>
2020-02-01 17:21:13 +0100

    my homework is done

--- TP2/answers.py
+++ TP2/answers.py
@@ -20,6 +20,8 @@ def answer_01(data):
     """
     # TODO : standardize data
     scaled_data = data
+    scaled_data = (data - data.mean()) / data.std()
+    scaled_data.iloc[:, 2] = data.iloc[:, 2]
     return scaled_data
 
 
@@ -32,9 +34,8 @@ def answer_02():
         nearest_neighbors class
     """
     # Wrong classifier
-    from sklearn.naive_bayes import GaussianNB
-
-    return GaussianNB
+    from sklearn.neighbors import KNeighborsClassifier
+    return KNeighborsClassifier
 
 
 def answer_03():
@@ -47,7 +48,7 @@ def answer_03():
     Returns
     -------
     """
-    from sklearn.metrics import accuracy_score as sklearn_metric
+    from sklearn.metrics import balanced_accuracy_score as sklearn_metric
     return sklearn_metric
 
 
@@ -64,7 +65,7 @@ def answer_04():
     YES = 'YES'
     NO = 'NO'
     # Return YES or NO
-    return None
+    return NO
 
 
 def answer_05():
@@ -80,7 +81,7 @@ def answer_05():
     CASE3 = "CS"
     CASE4 = "CROP"
     # Return CASE1, CASE2, CASE3 or CASE4
-    return None
+    return CASE3
 
 
 
@@ -92,7 +93,7 @@ def answer_06():
     under_fitting = "under-fitting"
     over_fitting  = "over-fitting"
     # Return under_fitting or over_fitting
-    return None
+    return over_fitting
 
 
 def answer_07():
@@ -102,7 +103,7 @@ def answer_07():
     under_fitting = "under-fitting"
     over_fitting  = "over-fitting"
     # Return under_fitting or over_fitting
-    return None
+    return under_fitting
 
 
 def answer_08():
@@ -113,7 +114,7 @@ def answer_08():
          "Decision Tree", "Random Forest", "Neural Net", "AdaBoost",
          "Naive Bayes", "QDA"]
 
-    overfitting_models = ["Replace", "with", "model", "names", "from", "the",  "list", "above"]
+    overfitting_models = ['RBF SVM', 'Decision Tree', 'Random Forest']
     return overfitting_models
 
 
@@ -125,7 +126,7 @@ def answer_09():
          "Decision Tree", "Random Forest", "Neural Net", "AdaBoost",
          "Naive Bayes", "QDA"]
 
-    underfitting_models = ["Replace", "with", "model", "names", "from", "the", "list", "above"]
+    underfitting_models = ['Linear SVM', 'Naive Bayes']
     return underfitting_models
 
 
